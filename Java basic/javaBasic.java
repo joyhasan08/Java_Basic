f0b9cc628574a357102d14e4bfdd0e3b54b45b82
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2024-08-29 00:04:09 +0600

    build 0.0.2

---
+++ Java basic/javaBasic.java
@@ -0,0 +1,15 @@
+// /**
+//  * javaBasic
+//  */
+// public class javaBasic {
+
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//     }
+// }
+
+class test {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA");
+    }
+}
