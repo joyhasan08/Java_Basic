9590cc2eef6265f7f1426d841924ea017083c984
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2024-08-29 00:40:08 +0600

    java basic

--- Java basic/javaBasic.java
+++ Java basic/javaBasic.java
@@ -1,15 +1,9 @@
-// /**
-//  * javaBasic
-//  */
-// public class javaBasic {
+/**
+ * javaBasic
+ */
+public class javaBasic {
 
-//     public static void main(String[] args) {
-//         System.out.println("Hello World!");
-//     }
-// }
-
-class test {
     public static void main(String[] args) {
-        System.out.println("Hello JAVA");
+        System.out.println("Hello World!");
     }
 }
