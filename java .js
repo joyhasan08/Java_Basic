af700308007be04c2feed0794a4613d5a6ac2a04
nahid-diu-cse <nahid.diu.cse@gmail.com>
2024-10-01 16:32:15 +0600

    portfolio update 2.0

---
+++ java .js
@@ -0,0 +1,14 @@
+console.log('hello world');
+
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(1, 2))  
+if (true) {
+    console.log('hello')
+}               
+else {
+    console.log('world')
+}   
